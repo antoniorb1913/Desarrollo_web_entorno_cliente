3da2898ca87a8d44c2d624151545db593c2d50f4
antoniorb1913 <antoniorb1913@gmail.com>
2025-11-05 20:49:52 +0100

    horario js

--- practica evaluable js/js/horario.js
+++ practica evaluable js/js/horario.js
@@ -1,60 +1,27 @@
-function horarioManana() {
-    const diasManana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"];
-    const horasManana = ["9:00-11:00", "11:00-13:00", "13:00-15:00"];
+function generarHorarios() {
+            let html = "<h3>Horario Mañana</h3><table>";
+            html += "<tr><th>Hora</th><th>Lunes</th><th>Martes</th><th>Miercoles</th><th>Jueves</th><th>Viernes</th></tr>";
+            
+            for (let h = 9; h <= 13; h += 2) {
+                html += "<tr><th>" + h + ":00-" + (h+2) + ":00</th>";
+                for (let d = 0; d < 5; d++) {
+                    html += "<td></td>";
+                }
+                html += "</tr>";
+            }
+            html += "</table>";
 
-    let html = "<h2>Horario de Mañana</h2>";
-    html += "<table border='1' style='width:100%; text-align:center; margin-bottom:30px;'>";
-    html += "<tr><th></th>";
+            html += "<h3>Horario Tarde</h3><table>";
+            html += "<tr><th>Hora</th><th>Lunes</th><th>Martes</th><th>Miercoles</th><th>Jueves</th><th>Viernes</th><th>Sabado</th><th>Domingo</th></tr>";
+            
+            for (let h = 16; h <= 20; h++) {
+                html += "<tr><th>" + h + ":00-" + (h+1) + ":00</th>";
+                for (let d = 0; d < 7; d++) {
+                    html += "<td></td>";
+                }
+                html += "</tr>";
+            }
+            html += "</table>";
 
-    for (let i = 0; i < diasManana.length; i++) {
-        html += "<th>" + diasManana[i] + "</th>";
-    }
-    html += "</tr>";
-
-    for (let i = 0; i < horasManana.length; i++) {
-        html += "<tr>";
-        html += "<th>" + horasManana[i] + "</th>";
-
-        for (let j = 0; j < diasManana.length; j++) {
-            html += "<td></td>";
+            document.getElementById("resultadoH").innerHTML = html;
         }
-        html += "</tr>";
-    }
-
-    html += "</table>";
-
-    document.getElementById("resultadoH").innerHTML = html;
-}
-
-
-function horarioTarde() {
-    const diasTarde = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
-    const horasTarde = [];
-
-    for (let h = 16; h < 21; h++) {
-        horasTarde.push(h + ":00-" + (h + 1) + ":00");
-    }
-
-    let html = "<h2>Horario de Tarde</h2>";
-    html += "<table border='1' style='width:100%; text-align:center;'>";
-    html += "<tr><th></th>";
-
-    for (let i = 0; i < diasTarde.length; i++) {
-        html += "<th>" + diasTarde[i] + "</th>";
-    }
-    html += "</tr>";
-
-    for (let i = 0; i < horasTarde.length; i++) {
-        html += "<tr>";
-        html += "<th>" + horasTarde[i] + "</th>";
-
-        for (let j = 0; j < diasTarde.length; j++) {
-            html += "<td></td>";
-        }
-        html += "</tr>";
-    }
-
-    html += "</table>";
-
-    document.getElementById("resultadoH").innerHTML = html;
-}
