24fefdec4fae6ce7b553a95dde58256ce1076c3f
antoniorb1913 <antoniorb1913@gmail.com>
2026-01-14 22:11:05 +0100

    tarea 2

--- REACT/nivel1_react/my-react-app/src/App.jsx
+++ REACT/nivel1_react/my-react-app/src/App.jsx
@@ -1,5 +1,37 @@
 function App() {
-  return <h1>Nivel 1 de React desbloqueado</h1>
+  // Comenatrio antes del return punto 4.11
+  return (
+    <div>
+      {/* Comenatrio punto 4.12 */}
+      <h1>Nivel 1 de React desbloqueado</h1>
+      <h1>Luego cambia el div por un Fragment vacío: <> ... </> y guarda.</h1>
+
+      <ParentComponent/>
+    </div>
+  );
+  
+}
+function ParentComponent() {
+  return (
+    <>
+      <UserComponent />
+      <ProfileComponent />
+      <FeedComponent />
+    </>
+  );
 }
 
-export default App
+function UserComponent() {
+  return <h2>User component</h2>;
+}
+
+function ProfileComponent() {
+  return <h2>Profile component</h2>;
+}
+
+function FeedComponent() {
+  return <h2>Feed component</h2>;
+}
+
+export default App;
+
