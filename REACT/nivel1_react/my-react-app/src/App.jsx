db23084aab065f2b65cc33e8ccf5000368bbdf06
antoniorb1913 <antoniorb1913@gmail.com>
2026-01-22 00:17:59 +0100

    Tarea6-React

--- REACT/nivel1_react/my-react-app/src/App.jsx
+++ REACT/nivel1_react/my-react-app/src/App.jsx
@@ -1,28 +1,21 @@
-import { useState } from "react"
+import Panel from './Panel'
+import Contador from './Contador'
 
-  export default function App() {
-
-    const [user, setUser] = useState(null)
-    const [newEmails, setNewEmails] = useState(0)
-
-    const button = user
-      ? <button onClick={() => setUser(null)}>Logout</button>
-      : <button onClick={() => setUser({ name: 'Antoñico'
-})}>Login</button>
-
-    return (
-      <>
-      <h1>Nivel 5: Renderizado condicional</h1>
-      {button}
-      {user ? <p>Bienvenido, {user.name}.</p> : <p>Inicia sesión para
-      continuar.</p>}
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Mi Panel de Control</h1>
+      
+      {/* Llamamos al componente Panel (Input + Mostrar/Ocultar) */}
+      <Panel />
+      
+      {/* Línea divisoria para separar las secciones */}
       <hr />
-      <button onClick={() => setNewEmails((n) => n + 1)}>+1
-      email</button>
-      <button onClick={() => setNewEmails(0)}>Reset</button>
-      {newEmails > 0 && (
-      <h2>Tienes {newEmails} correos nuevos.</h2>
-      )}
-      </>
+      
+      {/* Llamamos al componente Contador (+1 / -1) */}
+      <Contador />
+    </div>
   )
 }
+
+export default App
